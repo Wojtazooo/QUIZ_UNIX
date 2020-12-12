4703c6aa9dda40c4534ebd6f0d69cabbad1e33c9
Wojtazooo <wojtekulaski@gmail.com>
2020-12-12 17:30:30 +0100

    create quiz mode with getline

--- Quiz.c
+++ Quiz.c
@@ -43,7 +43,6 @@ void read_arg_quizmode(int argc, char **argv, int *questions, int* time, char **
     *path = malloc(sizeof(char)*MAX_PATH); 
     int is_p_set = 0;
 
-    printf("tryb rozwiązywania quizu\n"); 
     char c;
     optind = 2; // we ignore first one which is work_mode
     while ((c = getopt(argc, argv, "p:n:t:")) != -1)
@@ -70,12 +69,12 @@ void read_arg_quizmode(int argc, char **argv, int *questions, int* time, char **
     }    
     if(!is_p_set)
     {
-        printf("Path is madatory!\n");
+        printf("Path is mandatory!\n");
         usage(argv[0]);
     }   
     if(!is_valid_extensions(*path))
     {
-        printf("File should be %s extension\n", QUIZFILE_EXT);
+        printf("File should have %s extension\n", QUIZFILE_EXT);
         usage(argv[0]);
     } 
 }
@@ -106,66 +105,97 @@ void read_arg_createmode(int argc, char **argv, char **path, char **dir_path)
     }    
     if(!is_p_set)
     {
-        fprintf(stderr,"Path is madatory!\n");
+        fprintf(stderr,"Path is mandatory!\n");
         usage(argv[0]);
     }
 
     if(!is_valid_extensions(*path))
     {
-        printf("File should be %s extension\n", QUIZFILE_EXT);
+        printf("File should have %s extension\n", QUIZFILE_EXT);
         usage(argv[0]);
     }
 }
 
 void quiz_mode(int argc, char **argv)
 {
+    // read arguments of quiz mode
     int questions;
     int time;
     char *path;
     read_arg_quizmode(argc,argv,&questions,&time,&path);
-    //printf("Odczytane parametry:\npath = %s\n%d questions\n%d seconds \n",path, questions, time);     
+
+    // welcome with $USER environment variable
     char *name = getenv("USER");
     if(name) printf("Welcome %s in quiz mode!\n",name);
     else ERR("getenv");
+
+    
+}
+
+void add_question(char* path, char *word, char* translation)
+{
+    // create question line
+    char *buf = malloc(sizeof(char)*(2*MAX_COMMAND_SIZE + 1)); 
+    strcpy(buf,word);
+    strcat(buf," ");
+    strcat(buf,translation);
+    strcat(buf,"\n");
+
+    // adding to file
+    int out;
+    if((out = open(path,O_CREAT|O_RDWR|O_APPEND,0777)) < 0) ERR("open");
+    if(write(out,buf,strlen(buf)) <= 0) ERR("write");
+    if(close(out)) ERR("close");
+}
+
+int is_question_new(char *path, char *word)
+{
+    char *line = malloc(sizeof(char)*MAX_COMMAND_SIZE);
+    size_t len = 0;
+    FILE* stream = fopen(path,"r");
+    if(!stream) return 1; // if file doesnt exist
+
+    while(getline(&line,&len, stream) != -1)
+    {
+        if(strncmp(line,word,strlen(word)) == 0) // question already in this quiz file
+        {
+            fclose(stream);
+            return 0; 
+        }
+    }
+    return 1;
 }
 
 void create_quiz_mode(int argc, char **argv)
 {
+    // read arguments
     char *path;
     char *dir_path;
     read_arg_createmode(argc,argv,&path,&dir_path);
-    //printf("Odczytane arg: \npath = %s\noptional dir_path = %s\n",path,dir_path);
-    
     char *name = getenv("USER");
     if(name) printf("Welcome %s in create quiz mode!\n",name);
     else ERR("getenv");
 
-
+    // add new word and translation to quiz file
     char english_word[MAX_COMMAND_SIZE];
-    char translation[MAX_COMMAND_SIZE];
-    
-    int out;
-    char *buf = malloc(sizeof(char)*(2*MAX_COMMAND_SIZE + 1)); 
-
+    char translation[MAX_COMMAND_SIZE];  
     printf("[word translation] (type exit to exit)\n");
-    while(1)
+    while(1) // exit after "exit" input
     {
         if(fscanf(stdin, "%s", english_word) < 0) ERR("fscanf");  
         if(strcmp(english_word, "exit") == 0) break;
         if(fscanf(stdin, "%s", translation) < 0) ERR("fscanf");  
         if(strcmp(translation, "exit") == 0) break;
-        printf("[%s %s]\n", english_word, translation);
+        //printf("[%s %s]\n", english_word, translation);
 
-        strcpy(buf,english_word);
-        strcat(buf," ");
-        strcat(buf,translation);
-        strcat(buf,"\n");
-
-
-        if((out = open(path,O_CREAT|O_RDWR|O_APPEND,0777)) < 0) ERR("open");
-        if(write(out,buf,strlen(buf)) <= 0) ERR("write");
-        if(close(out)) ERR("close");
-
+        // if a question is new we will add it to file
+        if(is_question_new(path,english_word))
+        {
+            add_question(path,english_word,translation);
+            printf("[%s %s] was added\n", english_word, translation);
+        }
+        else
+            printf("Translation of %s already exists in %s\n", english_word, path);    
     }
 
 
